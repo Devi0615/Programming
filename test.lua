e575a11efe23a9ff49f4512c73559c5caf69277b
Devi0615 <devireddy0615@gmail.com>
2018-03-03 11:36:56 -0600

    Initial commit

---
+++ test.lua
@@ -0,0 +1,11 @@
+
+local k = 20
+local l = 5
+local tbl = {k, l}
+print(tbl)
+print(#tbl) -- Get the size of the table
+print(tbl[1]) -- Get a entry out of a table based on the index table[index]
+
+for i = 1, #tbl do
+print(tbl[i])
+end
